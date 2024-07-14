54fad57d61c0fa26eaa8fe2f783a78f29106b618
Kevin Hyde <kphyde@gmail.com>
2024-07-14 12:51:12 -0700

    Simplify example

--- 04-Intro_to_Vue/01-state-variables/src/App.vue
+++ 04-Intro_to_Vue/01-state-variables/src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
 import { ref } from "vue";
 
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-
-const text = ref("Hello!");
+const text = ref("Hello Buddy!");
 </script>
 
 <template>
@@ -17,14 +14,7 @@ const text = ref("Hello!");
       height="125"
     />
     <h1>{{ text }}</h1>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
