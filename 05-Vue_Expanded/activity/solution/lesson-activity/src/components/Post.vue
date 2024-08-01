c014baf3400e414495834f828b8c582cf36d3fe9
Kevin Hyde <kphyde@gmail.com>
2024-07-31 20:42:51 -0700

    update module 05 activity

---
+++ 05-Vue_Expanded/activity/solution/lesson-activity/src/components/Post.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  author: String,
+  title: String,
+  category: String,
+  excerpt: String,
+  likes: Number
+})
+
+const likes = ref(props.likes)
+
+const incrementLikes = () => {
+  likes.value++
+}
+</script>
+
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <h3>Category: {{ category }}</h3>
+    <p>{{ excerpt }}</p>
+    <p>Likes: {{ likes }}</p>
+    <button @click="incrementLikes">Like</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
